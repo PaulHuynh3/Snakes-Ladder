afe574ba7c45bbffc0f6f37c80e7fbb3c215e73b
Paul <paul.huynh3@gmail.com>
2017-09-09 13:33:34 -0400

    begin

---
+++ Snakes & Ladders/Snakes & Ladders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Snakes & Ladders
+//
+//  Created by Paul on 2017-09-09.
+//  Copyright © 2017 Paul. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
